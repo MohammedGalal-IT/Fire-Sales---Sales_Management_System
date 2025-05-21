80f835e7add3869c310e0bf6033a5060b63e884e
Mohammed Galal <MohammedGalal7777@hotmail.com>
2025-05-21 22:51:27 +0300

    Created All Models

---
+++ Fire Sales - Sales Management System/Model/Expense.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fire_Sales___Sales_Management_System.Model
+{
+    public enum ExpenseType
+    {
+        كهرباء,
+        أكل,
+        قات,
+        شيء_آخر
+    }
+
+    public class Expense
+    {
+        public int ExpenseID { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public ExpenseType ExpenseType { get; set; }
+        public int CreatedBy { get; set; }
+    }
+
+}
---
+++ Fire Sales - Sales Management System/Model/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fire_Sales___Sales_Management_System.Model
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public int CategoryID { get; set; }
+        public decimal CostPrice { get; set; }
+        public decimal SellingPrice { get; set; }
+        public int StockQuantity { get; set; }
+        public string Unit { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+}
