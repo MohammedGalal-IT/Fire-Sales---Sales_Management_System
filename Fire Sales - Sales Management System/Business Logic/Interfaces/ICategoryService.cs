dcca7afafb4e9ded5cf6cb2da4141be9da720c31
Mohammed Galal <MohammedGalal7777@hotmail.com>
2025-05-22 17:57:22 +0300

    Created CategoryService

---
+++ Fire Sales - Sales Management System/Business Logic/Interfaces/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fire_Sales___Sales_Management_System.Business_Logic.Interfaces
+{
+    interface ICategoryService
+    {
+    }
+}
---
+++ Fire Sales - Sales Management System/Business Logic/Services/CategoryService.cs
@@ -0,0 +1,47 @@
+﻿using Fire_Sales___Sales_Management_System.Business_Logic.Interfaces;
+using Fire_Sales___Sales_Management_System.Data_Access.Interfaces;
+using Fire_Sales___Sales_Management_System.Data_Access.Repositories;
+using Fire_Sales___Sales_Management_System.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fire_Sales___Sales_Management_System.Business_Logic.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return _categoryRepository.GetAll();
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            return _categoryRepository.GetById(id);
+        }
+
+        public bool CreateCategory(Category category)
+        {
+            return _categoryRepository.Add(category) > 0;
+        }
+
+        public bool UpdateCategory(Category category)
+        {
+            return _categoryRepository.Update(category) > 0;
+        }
+
+        public bool DeleteCategory(int id)
+        {
+            return _categoryRepository.Delete(id) > 0;
+        }
+    }
+}
