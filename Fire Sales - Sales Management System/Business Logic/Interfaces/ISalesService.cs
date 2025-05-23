f292a8cf8bedf506f823e88c06e313e9e51eed86
Mohammed Galal <MohammedGalal7777@hotmail.com>
2025-05-23 19:28:07 +0300

    Created Sales Service

---
+++ Fire Sales - Sales Management System/Business Logic/Interfaces/ISalesService.cs
@@ -0,0 +1,32 @@
+﻿using Fire_Sales___Sales_Management_System.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fire_Sales___Sales_Management_System.Business_Logic.Interfaces
+{
+    public interface ISalesService
+    {
+        // SalesInvoice Operations
+        List<SalesInvoice> GetAllInvoices();
+        SalesInvoice GetInvoiceById(int id);
+        SalesInvoice GetByInvoiceNumber(string invoiceNumber); 
+        List<SalesInvoice> GetInvoicesByUserId(int userId);     
+        List<SalesInvoice> GetInvoicesByDateRange(DateTime start, DateTime end);
+        bool CreateInvoice(SalesInvoice invoice);
+        bool UpdateInvoice(SalesInvoice invoice);
+        bool DeleteInvoice(int id);
+
+
+        // SalesItem Operations
+        SalesItem GetItemById(int itemId);
+        List<SalesItem> GetItemsByInvoiceId(int invoiceId);
+        List<SalesItem> GetItemsByProductId(int productId);
+        bool AddSalesItem(SalesItem item);
+        bool UpdateSalesItem(SalesItem item);
+        bool DeleteSalesItem(int itemId);
+    }
+
+}
