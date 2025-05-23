ced940a415e3e68dce0f5e4853d800b2ebbca0a6
AlSamadi Apps <98705258+Al-Samadi@users.noreply.github.com>
2025-05-23 22:28:39 +0300

    LoginView
    اضافة واجهة صفحة تسجيل الدخول LoginView

--- Fire Sales - Sales Management System/Presentation/Views/LoginView.Designer.cs
+++ Fire Sales - Sales Management System/Presentation/Views/LoginView.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Fire_Sales___Sales_Management_System.Presentation.Views
+﻿namespace LoginView
 {
-    partial class LoginView
+    partial class signinForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,36 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.logoPB = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.signinButton = new Guna.UI2.WinForms.Guna2Button();
+            this.passwordTB = new Guna.UI2.WinForms.Guna2TextBox();
+            this.usernameTB = new Guna.UI2.WinForms.Guna2TextBox();
+            this.titleLBL = new System.Windows.Forms.Label();
+            this.subtitleLBL = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPB)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // mainPanel
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(67, 77);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 26;
-            this.dataGridView1.Size = new System.Drawing.Size(587, 362);
-            this.dataGridView1.TabIndex = 0;
+            this.mainPanel.Controls.Add(this.logoPB);
+            this.mainPanel.Controls.Add(this.signinButton);
+            this.mainPanel.Controls.Add(this.passwordTB);
+            this.mainPanel.Controls.Add(this.usernameTB);
+            this.mainPanel.Controls.Add(this.titleLBL);
+            this.mainPanel.Controls.Add(this.subtitleLBL);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(600, 769);
+            this.mainPanel.TabIndex = 1;
+            this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
             // 
-            // LoginView
+            // logoPB
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.logoPB.FillColor = System.Drawing.Color.Black;
+            this.logoPB.Image = global::LoginView.Properties.Resources.img;
+            this.logoPB.ImageRotate = 0F;
+            this.logoPB.Location = new System.Drawing.Point(252, 100);
+            this.logoPB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.logoPB.Name = "logoPB";
+            this.logoPB.Size = new System.Drawing.Size(98, 87);
+            this.logoPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logoPB.TabIndex = 15;
+            this.logoPB.TabStop = false;
+            // 
+            // signinButton
+            // 
+            this.signinButton.BorderRadius = 10;
+            this.signinButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.signinButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.signinButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.signinButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.signinButton.FillColor = System.Drawing.Color.Tan;
+            this.signinButton.Font = new System.Drawing.Font("Cairo", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.signinButton.ForeColor = System.Drawing.Color.White;
+            this.signinButton.Location = new System.Drawing.Point(153, 552);
+            this.signinButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.signinButton.Name = "signinButton";
+            this.signinButton.Size = new System.Drawing.Size(294, 50);
+            this.signinButton.TabIndex = 14;
+            this.signinButton.Text = "SignIn";
+            this.signinButton.Click += new System.EventHandler(this.Guna2Button1_Click);
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.Animated = true;
+            this.passwordTB.BorderRadius = 10;
+            this.passwordTB.BorderThickness = 0;
+            this.passwordTB.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.passwordTB.DefaultText = "";
+            this.passwordTB.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.passwordTB.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.passwordTB.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.passwordTB.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.passwordTB.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.passwordTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.passwordTB.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.passwordTB.ForeColor = System.Drawing.Color.Black;
+            this.passwordTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.passwordTB.Location = new System.Drawing.Point(75, 440);
+            this.passwordTB.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.PasswordChar = '.';
+            this.passwordTB.PlaceholderForeColor = System.Drawing.Color.Gray;
+            this.passwordTB.PlaceholderText = "Password";
+            this.passwordTB.SelectedText = "";
+            this.passwordTB.Size = new System.Drawing.Size(450, 69);
+            this.passwordTB.TabIndex = 13;
+            // 
+            // usernameTB
+            // 
+            this.usernameTB.Animated = true;
+            this.usernameTB.BorderRadius = 10;
+            this.usernameTB.BorderThickness = 0;
+            this.usernameTB.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.usernameTB.DefaultText = "";
+            this.usernameTB.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.usernameTB.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.usernameTB.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usernameTB.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usernameTB.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.usernameTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.usernameTB.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.usernameTB.ForeColor = System.Drawing.Color.Black;
+            this.usernameTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.usernameTB.Location = new System.Drawing.Point(75, 347);
+            this.usernameTB.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.usernameTB.Name = "usernameTB";
+            this.usernameTB.PlaceholderForeColor = System.Drawing.Color.Gray;
+            this.usernameTB.PlaceholderText = "UserName";
+            this.usernameTB.SelectedText = "";
+            this.usernameTB.Size = new System.Drawing.Size(450, 69);
+            this.usernameTB.TabIndex = 12;
+            // 
+            // titleLBL
+            // 
+            this.titleLBL.AutoSize = true;
+            this.titleLBL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.titleLBL.Font = new System.Drawing.Font("Cairo Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLBL.ForeColor = System.Drawing.Color.Tan;
+            this.titleLBL.Location = new System.Drawing.Point(194, 177);
+            this.titleLBL.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.titleLBL.Name = "titleLBL";
+            this.titleLBL.Size = new System.Drawing.Size(211, 72);
+            this.titleLBL.TabIndex = 4;
+            this.titleLBL.Text = "Get Started";
+            // 
+            // subtitleLBL
+            // 
+            this.subtitleLBL.AutoSize = true;
+            this.subtitleLBL.BackColor = System.Drawing.Color.Transparent;
+            this.subtitleLBL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.subtitleLBL.Font = new System.Drawing.Font("Cairo", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitleLBL.ForeColor = System.Drawing.Color.Tan;
+            this.subtitleLBL.Location = new System.Drawing.Point(208, 233);
+            this.subtitleLBL.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.subtitleLBL.Name = "subtitleLBL";
+            this.subtitleLBL.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
+            this.subtitleLBL.Size = new System.Drawing.Size(187, 49);
+            this.subtitleLBL.TabIndex = 3;
+            this.subtitleLBL.Text = "Login Page";
+            // 
+            // signinForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
-            this.Name = "LoginView";
-            this.Text = "LoginView";
-            this.Load += new System.EventHandler(this.LoginView_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(600, 769);
+            this.Controls.Add(this.mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.Name = "signinForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Page";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPB)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label titleLBL;
+        private System.Windows.Forms.Label subtitleLBL;
+        private Guna.UI2.WinForms.Guna2TextBox usernameTB;
+        private Guna.UI2.WinForms.Guna2TextBox passwordTB;
+        private Guna.UI2.WinForms.Guna2Button signinButton;
+        private Guna.UI2.WinForms.Guna2PictureBox logoPB;
     }
 }
+
--- Fire Sales - Sales Management System/Presentation/Views/LoginView.cs
+++ Fire Sales - Sales Management System/Presentation/Views/LoginView.cs
@@ -1,29 +1,44 @@
-﻿using Fire_Sales___Sales_Management_System.Helpers.Database_Helpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Fire_Sales___Sales_Management_System.Presentation.Views
+namespace LoginView
 {
-    public partial class LoginView: Form
+
+    public partial class signinForm : Form
     {
-        public LoginView()
+        public signinForm()
         {
             InitializeComponent();
-            //    DatabaseManager db = new DatabaseManager(@"Data Source=DESKTOP-KL322NI\MOHAMMEDGALAL; DataBase=LibraryDataBase;integrated Security=true;");
-            //    //int s = db.ExecuteNonQuery($"INSERT INTO Book([title],[author],[pages],[date]) values('sss','mmm',100,null)", null);
-            //    DataTable dt = db.ExecuteDataTable($"select * from Book", null);
-            //    this.dataGridView1.DataSource = dt;
-            //    //MessageBox.Show(""+s);
         }
 
-        private void LoginView_Load(object sender, EventArgs e)
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MetroButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Guna2Button1_Click(object sender, EventArgs e)
         {
 
         }
